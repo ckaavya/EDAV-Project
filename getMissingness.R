00ffc714ce9fa7c793140ee24f5adfe9707dbb64
Arman Uygur <uygurarman@gmail.com>
2017-04-19 22:49:09 -0400

    Add files via upload

---
+++ getMissingness.R
@@ -0,0 +1,25 @@
+
+rm(list=ls())
+
+# Load all the packages required for this assignment #
+packages <- c("vcdExtra", "ggplot2","tidyr","dplyr","ggmosaic","ggthemes","oce","viridis","mi",
+              "extracat")
+purrr::walk(packages, library, character.only = TRUE, warn.conflicts = FALSE); rm(packages)
+
+link = 'https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv?accessType=DOWNLOAD'
+df<-read.csv(url(link),na.strings=c("","NA"))
+
+df$DATE <- as.Date(df$DATE,format='%m/%d/%Y')
+df = df[df$DATE>='2013/01/01',]
+df = df[df$DATE<'2017/01/01',]
+df = df[order(df$DATE),]
+
+
+na_perc = as.data.frame(100* apply(df, 2, function(col)sum(is.na(col))/length(col)))
+na_perc$Var = rownames(na_perc)
+colnames(na_perc) = c("NA_Percentage","Variables")
+na_perc <- na_perc[order(na_perc$NA_Percentage),]
+plot = ggplot(na_perc,aes(y= NA_Percentage, x=reorder( Variables,NA_Percentage)))
+plot + geom_bar( stat="identity", position = "dodge", fill ="firebrick4")+coord_flip()+ theme_economist() + scale_colour_economist() +
+  scale_y_continuous()+ ggtitle("Percentage of Missing Values in NYC Vehicle Collisions Dataset \nby Variable Type") +ylab("Missing Value Percentage") + xlab("Variables")+ theme(plot.title=element_text(hjust=0.5),title=element_text(size=16, face="italic",colour = "black"))
+
